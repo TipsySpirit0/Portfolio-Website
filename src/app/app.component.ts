f5859de845b756db18a81695dea58612ede0c94c
Tobiloba <tobitobe66@gmail.com>
2024-11-15 22:33:43 +0100

    refactor: improve header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { AboutComponent } from './about/about.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, RouterLink],
+  imports: [RouterOutlet, HeaderComponent, RouterLink, AboutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
